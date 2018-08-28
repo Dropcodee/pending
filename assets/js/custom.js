df403d16ceda810c82a3b5cd231340cdda946557
Joshua-owolabi <primtech.co@gmail.com>
2018-08-28 06:15:45 +0100

    member registration page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
   $(".sidenav").sidenav();
+
+  // MEMBER REGISTRATION PANEL CHANGES
+  $("#myTab a").on("click", e => {
+    e.preventDefault();
+    $(this).tab("show");
+  });
 });
