44dc4d26dca4d0eef64de4a424e6e9fb86fb4705
Joshua-owolabi <primtech.co@gmail.com>
2018-09-03 19:39:11 +0100

    mortgage page

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -12,3 +12,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/custom.css">
 </head>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="assets/js/custom.js"></script>
